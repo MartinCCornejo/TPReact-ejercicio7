83505edea0b0f796a0a55e9dbd64e8a294d99e48
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-01 17:35:23 -0300

    Se hizo la estructura principal del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
+    <Header></Header>
+    <Container className="flex-grow-1"></Container>
+    <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-danger text-center text-light  py-3">
+      <h1 className="display-5">
+        TP React <br></br> Ejercicio 7 - Lista de empleados
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
