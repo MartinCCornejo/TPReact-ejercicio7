a1467e7b3bbde137fb37b21f2d99e156be339052
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-01 18:00:47 -0300

    Agregamos los componentes que usaremos en el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import EmpleadoList from './components/EmpleadoList';
 
 function App() {
 
   return (
     <>
     <Header></Header>
-    <Container className="flex-grow-1"></Container>
+    <Container className="flex-grow-1">
+      <EmpleadoList></EmpleadoList>
+    </Container>
     <Footer></Footer>
     </>
   )
---
+++ src/components/EmpleadoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+
+const EmpleadoList = () => {
+    return (
+        <ListGroup>
+            
+        </ListGroup>
+    );
+};
+
+export default EmpleadoList;
